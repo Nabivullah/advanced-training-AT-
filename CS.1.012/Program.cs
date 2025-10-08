8f8791460197374f3027cd108ba1fa382d770c06
Nabivullah <Syed.Nabivullah@esyasoft.com>
2025-10-08 12:09:29 +0530

    second task

---
+++ CS.1.012/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CS._1._012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create three tariffs using different constructors.
+            Tariff tariff1 = new Tariff("Domestic");
+            Tariff tariff2 = new Tariff("Commercial", 8.0);
+            Tariff tariff3 = new Tariff("Agricultural", -10.0, 100.0);
+
+            //For units=120, print computed bill for each.
+            int units = 120;
+            Console.WriteLine($"{tariff1.Name} : {tariff1.ComputeBill(units)}");
+            Console.WriteLine($"{tariff2.Name} : {tariff2.ComputeBill(units)}");
+            Console.WriteLine($"{tariff3.Name} : {tariff3.ComputeBill(units)}");
+        }
+    }
+}
---
+++ CS.1.012/Tariff.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CS._1._012
+{
+    internal class Tariff
+    {
+        public string Name=string.Empty;
+        double RatePerKwh, FixedCharge;
+
+        //Validate() that throws if rate <= 0 or fixed < 0. for strech goal
+        public void Validate()
+        {
+            if (RatePerKwh <= 0)
+            {
+                throw new ArgumentException("Rate must be greater than 0");
+            }
+            if (FixedCharge < 0)
+            {
+                throw new ArgumentException("Fixed charge cannot be negative");
+            }
+        }
+
+
+        //Tariff(string name) → defaults: rate=6.0, fixed=50.
+        public Tariff (string name)
+        {
+            Name = name;
+            RatePerKwh = 6.0;
+            FixedCharge = 50;
+            Validate();
+        }
+
+        //Tariff(string name, double rate) → defaults fixed= 50.
+        public Tariff (string name, double rate)
+        {   
+            Name = name;
+            RatePerKwh = rate;
+            FixedCharge = 50;
+            Validate();
+        }
+        //Tariff(string name, double rate, double fixedCharge).
+        public Tariff (string name, double rate, double fixedCharge)
+        {
+            Name = name;
+            RatePerKwh = rate;
+            FixedCharge= fixedCharge;
+            Validate();
+        }
+
+
+        //ComputeBill(int units) → units * rate + fixed.
+
+        public double ComputeBill(int units)
+        {
+            return units * RatePerKwh + FixedCharge;
+        }
+    }
+}
