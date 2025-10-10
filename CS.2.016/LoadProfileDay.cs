8d5ea6a2a89698cb459f9fe3cd9b64b6f132cc18
Nabivullah <Syed.Nabivullah@esyasoft.com>
2025-10-10 12:47:01 +0530

    sixth task

---
+++ CS.2.016/LoadProfileDay.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS._2._016
+{
+    internal class LoadProfileDay
+    {
+        public DateTime Date { get; }
+        public int[] HourlyKwh { get; } // length 24
+        bool flag = true;
+        public LoadProfileDay(DateTime date, int[] hourly)
+        {
+            // clone array; validate length == 24; values >= 0
+            if (hourly == null || hourly.Length != 24)
+            {
+                throw new ArgumentException("Hourly readings must be a non-null array of 24 integers.", nameof(hourly));// throws an error
+            }
+            foreach (int kwh in hourly)
+            {
+                if (kwh < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(hourly), "Hourly readings cannot contain negative values."); //throws an error
+                }
+            }
+            this.Date = date;
+            this.HourlyKwh = (int[])hourly.Clone();
+
+        }
+        public int Total() // method to get the total kwh used
+        {
+            return this.HourlyKwh.Sum();
+            
+        }
+
+        public int PeakHour() // method to find the hour in which there was peak energy usage 
+        {
+            int max = this.HourlyKwh[0];
+            int idx = 0;
+            for(int i=0;i<this.HourlyKwh.Length;i++)
+            {
+                if (this.HourlyKwh[i] > max) {  max = this.HourlyKwh[i]; idx = i; }
+            }
+            return idx;
+        }
+    }
+}
---
+++ CS.2.016/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CS._2._016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Build a valid HourlyKwh array; instantiate a day.
+            int[] demoHourlyKwh = {1,2,3,4,5,6,7,8,9,10,11,24,13,14,15,16,17,18,9,20,21,22,22,22};
+            DateTime demoDate = DateTime.Now;
+
+            LoadProfileDay NewLoadProfile = new LoadProfileDay (demoDate,demoHourlyKwh);
+
+            //Print Date, Total, PeakHour.
+
+            Console.WriteLine($"{demoDate} | Total: {NewLoadProfile.Total()} kWh | PeakHour: {NewLoadProfile.PeakHour()}");
+
+
+
+
+        }
+    }
+}
