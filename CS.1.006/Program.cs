791293f1d900b1a34bd285c544656def1f933ee1
Nabivullah <Syed.Nabivullah@esyasoft.com>
2025-10-16 12:42:52 +0530

    Basics Task 6

---
+++ CS.1.006/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CS._1._006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reads previous and current readings(in kWh).
+            Console.Write("Enter previous reading (kWh): ");
+            double previousReading = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter current reading (kWh): ");
+            double currentReading = Convert.ToDouble(Console.ReadLine());
+            double consumption = currentReading - previousReading; //Calculates the difference(consumption).
+            if (consumption < 0)        //Checks:If consumption< 0 → Invalid.
+            {
+                Console.WriteLine("Invalid");
+            }
+            else if (consumption == 0)   //If consumption == 0 → Possible outage.
+            {
+                Console.WriteLine($"Net Consumption: {consumption} ,Possible outage detected");
+            }
+            else if (consumption > 500)  //If consumption > 500 → High consumption alert.
+            {
+                Console.WriteLine($"Net Consumption: {consumption} ,High consumption alert!");
+            }
+            else if(consumption > 100 && consumption < 500)
+            {
+                Console.WriteLine($"Net Consumption: {consumption} ,Consumption within normal range.");
+            }
+
+            
+
+            
+
+            
+
+            
+
+           
+        }
+    }
+}
