939d80487ba0f62af3dd376daaa499e62108ab12
Nabivullah <Syed.Nabivullah@esyasoft.com>
2025-10-16 12:52:28 +0530

    Basics Task 7

---
+++ CS.2.007/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace CS._2._007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given a meterCategory("DOMESTIC", "COMMERCIAL", "AGRICULTURE"):
+            string meterCategory;
+            Console.WriteLine("Enter meter category (DOMESTIC, COMMERCIAL, AGRICULTURE): ");
+            meterCategory = Console.ReadLine().ToUpper();
+            int units;
+            Console.WriteLine("Enter number of units consumed: ");
+            units = Convert.ToInt32(Console.ReadLine());
+            double rate = 0.0;
+            double totalBill = 0.0;
+            switch (meterCategory) //Use switch to assign rate
+            {
+                case "DOMESTIC":
+                    rate = 6.0;
+                    totalBill=rate*units;
+                    break;
+                case "COMMERCIAL":
+                    rate = 8.5;
+                    totalBill = rate * units;
+                    break;
+                case "AGRICULTURE":
+                    rate = 3.0;
+                    totalBill = rate * units;
+                    break;
+                default:
+                    Console.WriteLine("Invalid meter category.");
+                    return;
+            }
+            Console.WriteLine($"Category: {meterCategory} | Rate: Rs.{rate} | Total Bill: Rs.{totalBill}");//Calculate total bill for given units.
+
+        }
+    }
+}
